368eb78bacfdf3eb37d4b36ab8b1f03c8c967aa9
Noah Friedman <noahfriedman2@gmail.com>
2021-02-17 11:53:03 -0500

    Add initial functionality and tests for factor function

--- api/factor.go
+++ api/factor.go
@@ -1,11 +1,23 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func init() {
 	funcs = append(funcs, Factor)
 }
 
 func Factor(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
 
+	if s := q.Get("degree"); s == "" {
+		http.Error(w, "ERROR: Missing required query parameter 'degree'", http.StatusExpectationFailed)
+	} else if d, e := strconv.Atoi(s); e != nil || d < 2 {
+		http.Error(w, "ERROR: Query parameter 'degree' must be an integer >= 2", http.StatusExpectationFailed)
+	} else {
+		fmt.Println(d)
+	}
 }
--- api/factor_test.go
+++ api/factor_test.go
@@ -1,9 +1,34 @@
 package api_test
 
 import (
+	"github.com/noahfriedman-ca/quick-factor/api"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"net/http/httptest"
 )
 
 var _ = Describe("the Factor function", func() {
+	var w *httptest.ResponseRecorder
+	BeforeEach(func() {
+		w = httptest.NewRecorder()
+	})
 
+	DescribeTable("when an error should be thrown",
+		func(queries string) {
+			r := httptest.NewRequest("", "http://example.com?"+queries, nil)
+
+			api.Factor(w, r)
+
+			b, e := ioutil.ReadAll(w.Result().Body)
+			Expect(e).NotTo(HaveOccurred())
+
+			Expect(string(b)).To(ContainSubstring("ERROR:"))
+		},
+		Entry("should throw an error when the 'degree' query isn't present", ""),
+		Entry("should throw an error when the 'degree' query isn't numeric", "degree=notanumber"),
+		Entry("should throw an error when the 'degree' query isn't an integer", "degree=3.14"),
+		Entry("should throw an error when the 'degree' query is < 2", "degree=1"),
+	)
 })
