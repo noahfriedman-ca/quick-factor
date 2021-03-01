fb2a5991bf6e62edb6376fc1524328ce0ab3dc55
Noah Friedman <noahfriedman2@gmail.com>
2021-03-01 10:48:11 -0500

    Add more implementation and testing for factor function [skip ci/cd]

--- api/factor.go
+++ api/factor.go
@@ -1,11 +1,14 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func init() {
@@ -14,11 +17,14 @@ func init() {
 
 // Struct defining the JSON response from the API function.
 type FactorJSON struct {
-	Result   string `json:"result"` // A string representing the factoring result; either "full", "partial", or "not"
-	Factored struct {
-		Expression string    `json:"expression"`
-		Intercepts []float64 `json:"intercepts,omitempty"`
-	} `json:"factored,omitempty"`
+	Result   string        `json:"result"` // A string representing the factoring result; either "full", "quadratic", "partial", or "not"
+	Factored *FactoredJSON `json:"factored,omitempty"`
+}
+
+// Struct defining the JSON representation of a factored polynomial.
+type FactoredJSON struct {
+	Expression string   `json:"expression"`
+	Intercepts []string `json:"intercepts,omitempty"`
 }
 
 // API function for factoring polynomials
@@ -37,12 +43,12 @@ func Factor(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Extract each exponent from query parameters
-	exps := make([]float64, degree+1)
-	for i := range exps {
+	coefficients := make([]float64, degree+1)
+	for i := range coefficients {
 		// Extra whitespace is trimmed to avoid unintentional errors
 		if s := strings.Trim(q.Get(fmt.Sprintf("x^%d", i)), " "); s == "" {
 			// If an empty string is found, assume a value of 0
-			exps[i] = 0
+			coefficients[i] = 0
 		} else if f, e := strconv.ParseFloat(s, 64); e != nil {
 			http.Error(w, fmt.Sprintf("ERROR: could not parse value in query parameter 'x^%d'", i), http.StatusExpectationFailed)
 			return
@@ -50,19 +56,117 @@ func Factor(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, fmt.Sprintf("ERROR: x^%d must not be 0", i), http.StatusExpectationFailed)
 			return
 		} else {
-			exps[i] = f
+			coefficients[i] = f
 		}
 	}
 
-}
-
-// The actual mathematical logic for this API function.
-func actualFactoring(degree uint, exponents ...float64) (uint, []float64) {
-	if degree < 2 {
-		// This shouldn't be a possible outcome, which is why panic is permitted
-		log.Panicln("degree is smaller than 2")
-		return 0, nil
+	// Do the actual factoring
+	var result *FactorJSON
+	if degree == 2 {
+		result = factorTrinomial(coefficients)
+	} else {
+		result = factorPolynomial(degree, coefficients)
 	}
 
-	return 0, nil
+	// Write the result
+	if b, e := json.Marshal(result); result == nil || e != nil {
+		http.Error(w, "ERROR: failed to factor", http.StatusInternalServerError)
+		if e == nil {
+			log.Println("nil result")
+		} else {
+			log.Println(e)
+		}
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write(b)
+	}
+}
+
+// Implements specific factoring rules that can only be applied to a 3-term polynomial.
+func factorTrinomial(coefficients []float64) *FactorJSON {
+	// Ensure that there are the correct number of coefficients
+	if len(coefficients) != 3 {
+		log.Println("factorTrinomial called with not exactly 3 coefficients")
+		return nil
+	}
+
+	// Check if any of the coefficients are not integers. If they are skip ahead to using the quadratic formula.
+	for _, v := range coefficients {
+		if v != math.Round(v) {
+			goto formula
+		}
+	}
+
+	// Attempt to factor by grouping
+
+formula:
+	// Calculate the components of the quadratic formula
+	var (
+		negativeB    = -1 * coefficients[1]
+		discriminant = math.Pow(coefficients[1], 2) - (4 * coefficients[0] * coefficients[2])
+		twoA         = 2 * coefficients[2]
+	)
+
+	// If the discriminant is negative, it has no square root, so do not factor further
+	if !(discriminant > 0) {
+		// Generate x-intercept strings
+		intercepts := make([]string, 2)
+		for i, v := range []byte{'+', '-'} {
+			intercepts[i] = fmt.Sprintf("(%f %c √(%f)) / %f", negativeB, v, discriminant, twoA)
+		}
+
+		return &FactorJSON{
+			Result: "quadratic",
+			Factored: &FactoredJSON{
+				Expression: fmt.Sprintf("(%s)(%s)", intercepts[0], intercepts[1]),
+				Intercepts: intercepts,
+			},
+		}
+	}
+
+	return nil
+}
+
+// Implements general factorization rules that can be applied to any polynomial.
+func factorPolynomial(degree uint, coefficients []float64) *FactorJSON {
+	return nil
+}
+
+// Calculate factors of 'x'. 'x' must be positive.
+func findFactorsOf(x uint) chan float64 {
+	var (
+		wg      sync.WaitGroup
+		factors = make(chan float64, x) // Buffer the maximum amount of values that can be sent, though it will likely never be necessary to have that many values in the channel
+	)
+
+	// Check that x is not 0
+	if x == 0 {
+		log.Println("there are no factors of 0, and this shouldn't have happened")
+		return nil
+	}
+
+	// Send '1' to the channel because everything can be factored by 1
+	factors <- 1
+
+	// Calculate if each value from 2 to x is a factor
+	go func() {
+		for i := uint(2); i < (x - 1); i++ {
+			wg.Add(1)
+			go func(i uint) {
+				defer wg.Done()
+				if f := float64(x) / float64(i); math.Round(f) == f {
+					factors <- f
+				}
+			}(i)
+		}
+	}()
+
+	// Ensure channel gets closed at the proper time
+	go func() {
+		wg.Wait()
+		close(factors)
+	}()
+
+	// Return the channel because it can be iterated over, no reason to convert to array
+	return factors
 }
--- api/factor_test.go
+++ api/factor_test.go
@@ -8,8 +8,10 @@ import (
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"io/ioutil"
+	"math"
 	"math/rand"
 	"net/http/httptest"
+	"strconv"
 )
 
 var _ = Describe("the Factor function", func() {
@@ -39,7 +41,7 @@ var _ = Describe("the Factor function", func() {
 	)
 
 	DescribeTable("the results when attempting to factor certain polynomials",
-		func(genPolynomial func() ([]float64, []float64), expected func(intercepts []float64) *api.FactorJSON) {
+		func(genPolynomial func() ([]float64, []string), expected func(intercepts []string) *api.FactorJSON) {
 			polynomial, intercepts := genPolynomial()
 			queries := fmt.Sprintf("degree=%d", len(polynomial)-1)
 			for i, v := range polynomial {
@@ -55,30 +57,30 @@ var _ = Describe("the Factor function", func() {
 			Expect(respJSON).To(Equal(*expected(intercepts)))
 		},
 		Entry("should factor 'x^3 - 2x^2 - 5x + 6' into '(x - 1)(x + 2)(x - 3)'",
-			func() ([]float64, []float64) {
+			func() ([]float64, []string) {
 				// No intercept array is returned because its not needed
 				return []float64{6, -5, -2, 1}, nil
 			},
-			func(_ []float64) *api.FactorJSON {
+			func(_ []string) *api.FactorJSON {
 				return &api.FactorJSON{
-					Result: "full",
-					Factored: struct {
-						Expression string    `json:"expression"`
-						Intercepts []float64 `json:"intercepts,omitempty"`
-					}{Expression: "(x - 1)(x + 2)(x - 3)", Intercepts: []float64{}},
+					Result:   "full",
+					Factored: &api.FactoredJSON{Expression: "(x - 1)(x + 2)(x - 3)", Intercepts: []string{"1", "-2", "3"}},
 				}
 			}),
 		Entry("should be unable to factor '2x^3 + 7x^2 + 4",
-			func() ([]float64, []float64) {
+			func() ([]float64, []string) {
 				// No intercept array is returned because its not needed
 				return []float64{4, 0, 7, 2}, nil
 			},
-			func(_ []float64) *api.FactorJSON {
+			func(_ []string) *api.FactorJSON {
 				return &api.FactorJSON{Result: "not"}
 			}),
 		Entry("should be able to factor a randomly generated factorable polynomial",
-			func() ([]float64, []float64) {
-				a := make([]float64, 3)
+			func() ([]float64, []string) {
+				var (
+					a    = make([]float64, 3)
+					ints = make([]string, 3)
+				)
 				for i := range a {
 					r := rand.Intn(8) + 1
 					b := rand.Intn(1) != 0
@@ -88,18 +90,19 @@ var _ = Describe("the Factor function", func() {
 					}
 
 					a[i] = float64(r)
+					ints[i] = strconv.Itoa(r)
 				}
 
 				// Expand (x - a0)(x - a1)(x - a2) into x^3 - (a0 + a1 + a2)x^2 + (a0a1 + (a0 + a1)a2)x - a0a1a2
 				expanded := []float64{a[0] * a[1] * a[2], (a[0] * a[1]) + ((a[0] + a[1]) * a[2]), a[0] + a[1] + a[2], 1}
 
-				return expanded, a
+				return expanded, ints
 			},
-			func(ints []float64) *api.FactorJSON {
+			func(ints []string) *api.FactorJSON {
 				// Extract proper operators based on intercept values
 				var ops [3]string
 				for i, v := range ints {
-					if v < 0 {
+					if v[0] == '-' {
 						ops[i] = "+"
 					} else {
 						ops[i] = "-"
@@ -109,11 +112,85 @@ var _ = Describe("the Factor function", func() {
 				// Define expected result
 				return &api.FactorJSON{
 					Result: "full",
-					Factored: struct {
-						Expression string    `json:"expression"`
-						Intercepts []float64 `json:"intercepts,omitempty"`
-					}{
-						Expression: fmt.Sprintf("(x %s %f)(x %s %f)(x %s %f)", ops[0], ints[0], ops[1], ints[1], ops[2], ints[2]),
+					Factored: &api.FactoredJSON{
+						Expression: fmt.Sprintf("(x %s %s)(x %s %s)(x %s %s)", ops[0], ints[0], ops[1], ints[1], ops[2], ints[2]),
+						Intercepts: ints,
+					},
+				}
+			},
+		),
+		Entry("should be able to solve using the quadratic formula",
+			func() ([]float64, []string) {
+				// No intercept array is returned because its not needed
+				return []float64{-2, 10, 1}, nil
+			},
+			func(_ []string) *api.FactorJSON {
+				return &api.FactorJSON{
+					Result: "quadratic",
+					Factored: &api.FactoredJSON{
+						Expression: "(x - 0.1962)(x + 10.1962)",
+						Intercepts: []string{"-10.1962", "0.1962"},
+					},
+				}
+			},
+		),
+		Entry("should factor into (-b ± √(b²-4ac)) ÷ 2a) form when the discriminant is negative",
+			func() ([]float64, []string) {
+				// Randomly generate coefficients
+				coefficients := make([]float64, 3)
+			generate:
+				for i := 0; i < 5; i++ {
+					for i := range coefficients {
+						coefficients[i] = float64(rand.Intn(1999)+1) / 100
+					}
+
+					// Check if b² is larger than or equal to 4ac. If it is, restart the loop.
+					if math.Pow(coefficients[1], 2) >= (4 * coefficients[0] * coefficients[2]) {
+						coefficients = make([]float64, 3)
+						continue generate
+					}
+
+					// Make sure that a trinomial with these coefficients cannot be factored by grouping
+					for _, v := range coefficients {
+						// If any of the coefficients are not whole numbers, the whole thing can't be factored by grouping
+						if v != math.Round(v) {
+							break generate
+						}
+					}
+
+					// If the loop is still running, all coefficients must be whole numbers
+					// Now, check if the trinomial can be factored by grouping
+					var (
+						product = coefficients[0] * coefficients[2]
+						half    = math.Ceil(coefficients[1] / 2)
+					)
+					for a, b := 1., coefficients[1]-1; a < half; a, b = a+1, b-1 {
+						if a*b == product {
+							// If the trinomial is factorable by grouping, reset the array and restart the loop
+							coefficients = make([]float64, 3)
+							continue generate
+						}
+					}
+				}
+
+				// If coefficients[0] is 0, then everything must be 0 (because 0 cannot be randomly generated)
+				if coefficients[0] == 0 {
+					Fail("took too long to generate coefficients")
+					return nil, nil
+				}
+
+				ints := make([]string, 2)
+				for i, v := range []byte{'+', '-'} {
+					ints[i] = fmt.Sprintf("(%f %c √(%f)) / %f", coefficients[1]*-1, v, math.Pow(coefficients[1], 2)-(4*coefficients[2]*coefficients[0]), 2*coefficients[2])
+				}
+
+				return coefficients, ints
+			},
+			func(ints []string) *api.FactorJSON {
+				return &api.FactorJSON{
+					Result: "quadratic",
+					Factored: &api.FactoredJSON{
+						Expression: fmt.Sprintf("(%s)(%s)", ints[0], ints[1]),
 						Intercepts: ints,
 					},
 				}
